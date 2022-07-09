4ab835f50cfd17f4e7cca671acc11ec36d278e95
Alexlin7 <alitw@outlook.com>
2022-07-09 18:26:36 +0800

    刪除未使用的import

--- src/main/java/com/alexlin7/demo/entity/Product.java
+++ src/main/java/com/alexlin7/demo/entity/Product.java
@@ -3,9 +3,6 @@ package com.alexlin7.demo.entity;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-
 @Document(collection = "products")
 @NoArgsConstructor
 @AllArgsConstructor
