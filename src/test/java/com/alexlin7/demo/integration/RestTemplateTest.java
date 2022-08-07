9f7dc419375cbc91b9fc64fe8021bd0edc3f82df
Alexlin7 <alitw@outlook.com>
2022-08-07 21:47:43 +0800

    RestTemplateTest fin

--- src/test/java/com/alexlin7/demo/integration/RestTemplateTest.java
+++ src/test/java/com/alexlin7/demo/integration/RestTemplateTest.java
@@ -6,16 +6,21 @@ import com.alexlin7.demo.entity.appUser.UserAuthority;
 import com.alexlin7.demo.entity.product.Product;
 import com.alexlin7.demo.entity.product.ProductRequest;
 import com.alexlin7.demo.entity.product.ProductResponse;
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -41,6 +46,7 @@ public class RestTemplateTest extends BaseTest {
         String url = domain + "/products/" + product.getId();
         ProductResponse productResponse = restTemplate.getForObject(url, ProductResponse.class);
 
+        Assert.assertNotNull(productResponse);
         Assert.assertEquals(product.getId(), productResponse.getId());
         Assert.assertEquals(product.getPrice(), productResponse.getPrice());
         Assert.assertEquals(product.getName(), productResponse.getName());
@@ -50,8 +56,8 @@ public class RestTemplateTest extends BaseTest {
     public void testGetProducts() {
         Product p1 = createProduct("Operation Management", 350);
         Product p2 = createProduct("Marketing Management", 200);
-        Product p3 = createProduct("Financial Statement Analysis", 400);
-        Product p5 = createProduct("Enterprise Resource Planning", 420);
+        createProduct("Financial Statement Analysis", 400);
+        createProduct("Enterprise Resource Planning", 420);
         Product p4 = createProduct("Human Resource Management", 440);
 
         String url = domain + "/products?keyword={name}&orderBy={orderField}&sortRule={sortDirection}";
@@ -106,6 +112,57 @@ public class RestTemplateTest extends BaseTest {
         return token;
     }
 
+    @Test
+    public void testUserAuthentication() {
+        AppUser appUser = createUser("Ireina", Collections.singletonList(UserAuthority.NORMAL));
+        obtainAccessToken(appUser.getEmailAddress());
+    }
+
+    @Test
+    public void testReplaceProduct() {
+        AppUser appUser = createUser("Ireina", Collections.singletonList(UserAuthority.NORMAL));
+        Product product = createProduct("Game", 100);
+
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setName("Game;");
+        productRequest.setPrice(120);
+
+        String token = obtainAccessToken(appUser.getEmailAddress());
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, token);
+
+        HttpEntity<ProductRequest> httpEntity = new HttpEntity<>(productRequest, httpHeaders);
+        String url = domain + "/products/" + product.getId();
+        ResponseEntity<ProductResponse> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, httpEntity, ProductResponse.class);
+
+        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        ProductResponse productResponse = responseEntity.getBody();
+        Assert.assertNotNull(productResponse);
+        Assert.assertEquals(productRequest.getName(), productResponse.getName());
+        Assert.assertEquals(productRequest.getPrice().intValue(), productResponse.getPrice());
+    }
+
+    @Test
+    public void testExchangeRateAPI() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.TEXT_HTML));
+
+        restTemplate = new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(10))
+                .additionalMessageConverters(converter)
+                .build();
+
+        String url = "https://www.freeforexapi.com/api/live?pairs=USDTWD,USDEUR";
+        HttpEntity<Void> httpEntity = new HttpEntity<>(null, null);
+        ResponseEntity<ExchangeRateResponse> resEntitiy = restTemplate
+                .exchange(url, HttpMethod.GET, httpEntity,
+                        new ParameterizedTypeReference<ExchangeRateResponse>() {});
+
+        ExchangeRateResponse exRateRes = resEntitiy.getBody();
+        Assert.assertNotNull(exRateRes);
+        Assert.assertNotNull(exRateRes.getRates().get("USDEUR"));
+        Assert.assertNotNull(exRateRes.getRates().get("USDTWD"));
+    }
+
     private Product createProduct(String name, int price) {
         Product product = new Product();
         product.setName(name);
@@ -113,4 +170,19 @@ public class RestTemplateTest extends BaseTest {
 
         return productRepository.insert(product);
     }
+
+    @Getter
+    @Setter
+    public static class ExchangeRateResponse {
+        private Map<String, RateData> rates;
+        private int code;
+    }
+
+    @Getter
+    @Setter
+    public static class RateData {
+        private double rate;
+        private long timestamp;
+    }
+
 }
